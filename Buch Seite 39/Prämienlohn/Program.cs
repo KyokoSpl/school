94d0507a9439e9166fe05ac9d394b6c2fa8cd827
KyokoSpl <kieseelias@gmail.com>
2024-02-26 14:32:50 +0100

    fixed prämienlohn

--- Buch Seite 39/Prämienlohn/Program.cs
+++ Buch Seite 39/Prämienlohn/Program.cs
@@ -22,7 +22,7 @@ namespace Prämienlohn
 
             if( Stk > 150)
             {
-                Prämienstufe = Stk - 150 / 10;
+                Prämienstufe = (Stk - 150) / 10;
                 Prämie = Prämienstufe * 0.002;
                 Gesamtlohn = 5 * 8 + Prämie;
                 Console.WriteLine("Der gesamtlohn Beträgt {0}€, Die Prämie beträgt {1}€", Gesamtlohn, Prämie);
