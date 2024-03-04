a824085da06944b67dab82f7c4065ab577e4c40c
KyokoSpl <kieseelias@gmail.com>
2024-03-04 14:56:56 +0100

    fixed Autovermieter

--- Buch Seite 39/Autovermieter/Program.cs
+++ Buch Seite 39/Autovermieter/Program.cs
@@ -11,35 +11,34 @@ namespace Autovermieter
         static void Main(string[] args)
         {
             // Defining variables
-            int  normalpricebrutto = 68;
-            double normalpricenetto;
-            double kilometer;
-            double kmextra;
+            const int  normalpricebrutto = 68;
+            float normalpricenetto;
+            int kilometer;
+            int kmextra;
             string kminput;
-            double additional = 0.65;
-            double finalpricebrutto;
-            double finalpricenetto;
-            double mwst;
+            const float additional = 0.65f;
+            float finalpricebrutto;
+            float finalpricenetto;
+            const float mwst = 0.19f;
 
             // Get user input and covert to correct data type
             Console.WriteLine("price calc");
             Console.Write("please enter kilometer:");
             kminput = Console.ReadLine();
-            kilometer = Convert.ToDouble(kminput);
+            kilometer = Convert.ToInt32(kminput);
 
             if(kilometer > 200)
             {
                 // calculate price if they drove over 200km
                 kmextra = kilometer - 200;
                 finalpricebrutto = normalpricebrutto + additional * kmextra;
-                mwst = finalpricebrutto * 0.19;
-                finalpricenetto = finalpricebrutto + mwst;
+                finalpricenetto = finalpricebrutto + finalpricebrutto * mwst;
                 Console.WriteLine("your final price is {0} tax included. Taxes: {1}", finalpricenetto,mwst);
             }
             else
             {
                 // calculate price if they're in the normal price range
-                mwst = normalpricebrutto * 0.19;
+                normalpricenetto = normalpricebrutto * mwst;
                 normalpricenetto = normalpricebrutto + mwst;
                 
                 Console.WriteLine("your final price is {0} tax included. Taxes: {1}", normalpricenetto, mwst);
