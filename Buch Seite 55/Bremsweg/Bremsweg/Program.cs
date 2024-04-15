3d2977ecb8a7cedbd86837f58c78df0fb0682b17
KyokoSpl <kieseelias@gmail.com>
2024-04-15 14:47:39 +0200

    added Bremsweg project

---
+++ Buch Seite 55/Bremsweg/Bremsweg/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bitte geben Sie die Anzahl der Geschwindigkeiten ein:");
+        int numSpeeds = int.Parse(Console.ReadLine());
+
+        double acceleration = 8; // Bremsbeschleunigung in m/s²
+
+        for (int i = 1; i <= numSpeeds; i++)
+        {
+            Console.WriteLine($"Eingabe {i}: Geschwindigkeit {i}:");
+            double velocityKmh = Convert.ToDouble(Console.ReadLine());
+            double velocityMs = velocityKmh * 1000 / 3600; // Umrechnung von km/h in m/s
+            double brakingDistance = (velocityMs * velocityMs) / (2 * acceleration);
+            Console.WriteLine($"Bremsweg bei Geschwindigkeit {velocityKmh} km/h: {brakingDistance:F2} m \n");
+
+        }
+        Console.WriteLine("press any key to exit the programm");
+        Console.ReadKey();
+    }
+}
