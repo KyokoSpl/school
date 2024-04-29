0acc037560b02066e0870f4b9b47cc690a45c7de
KyokoSpl <kieseelias@gmail.com>
2024-04-29 14:24:45 +0200

    added S.52 NR 2 Autovermietung

---
+++ Buch Seite 52/Versandkosten/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versandkosten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] shippingcost_arr = new double[10] { 30, 40, 50, 60, 70, 80, 90, 69, 420, 666 };
+            double shippingcost;
+            string itemgroup_in;
+            
+
+            Console.WriteLine("geben sie die identnummer ihrer Warengruppe ein [1-10]");
+            itemgroup_in = Console.ReadLine();
+            int itemgroup = Convert.ToInt32(itemgroup_in);
+            shippingcost = shippingcost_arr[itemgroup-1];
+            Console.WriteLine("Die Versandcosten für Warengruppe:{0} betragen {1}€", itemgroup, shippingcost);
+            Console.ReadKey();
+        }
+    }
+}
